0a6d186eb5b94d013d5f57bd9dd7630e35bf4c43
Herbds <acabohorquez43@gmail.com>
2024-08-17 23:04:54 -0500

    Adición desencriptar funcionalidad

--- index.js
+++ index.js
@@ -25,3 +25,31 @@ function encriptar() {
     }
     
 }
+
+function desencriptar() {
+    let tituloMensaje = document.getElementById("titulo-mensaje").value;
+    let texto = document.getElementById("texto").value;
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+
+    let textoCifrado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ober/gi, "o")
+        .replace(/ai/gi, "a")
+        .replace(/ufat/gi, "u");
+
+    if (document.getElementById("texto").value.length !=0) {
+
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent="Texto encriptado con exito";
+        parrafo.textContent="";
+        muñeco.src=".\imagenes\Binario.jpeg";
+    }   else {
+        muñeco.src=".\imagenes\alerta.png";
+        tituloMensaje.textContent="Ningún mensaje fue encontrado";
+        parrafo.textContent="Ingresa el texto que deseas encriptar o desencriptar"
+        alert("Debes ingresar el texto");
+    }
+    
+}
